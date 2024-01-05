ab541f0a0cceb00778fea7778b17888b660bcf94
MikhailTapio <mikhailtapio13468@163.com>
2024-01-05 22:03:16 +0800

    Try to fix potential cleanroom conflict

--- src/main/java/committee/nova/persistcd/mixin/AccessorCoolDown.java
+++ src/main/java/committee/nova/persistcd/mixin/AccessorCoolDown.java
@@ -2,13 +2,16 @@ package committee.nova.persistcd.mixin;
 
 import net.minecraft.util.CooldownTracker;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(CooldownTracker.Cooldown.class)
 public interface AccessorCoolDown {
+    @Mutable
     @Accessor
     void setCreateTicks(int startTime);
 
+    @Mutable
     @Accessor
     void setExpireTicks(int endTime);
 
